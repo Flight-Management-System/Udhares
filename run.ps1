f71a105350f6f7f7fd1b0c58b9fc6a6759a6e896
Yaseen <ahm3d.yaseen@gmail.com>
2024-02-15 15:57:22 +0500

    misc: remove unncessary run scripts

--- run.ps1
+++
@@ -1,5 +0,0 @@
-# Start the Vue.js application in the 'udhares' directory
-Start-Process npm -ArgumentList "run serve" -NoNewWindow -WorkingDirectory ".\udhares\" -PassThru
-
-# Start the ASP.NET project in the 'API' directory
-Start-Process dotnet -ArgumentList "run" -NoNewWindow -WorkingDirectory ".\API\" -PassThru
